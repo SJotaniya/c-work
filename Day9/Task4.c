68e88db5cc44601f6c30ef4afe1a4982f0dca9b2
SJotaniya <saurabhjotaniya6531@outlook.com>
2024-03-20 15:29:09 +0530

    work

--- Day9/Task4.c
+++ Day9/Task4.c
@@ -1,2 +1,22 @@
-// Program to print the Fibonacci series up to N numbers using a for loop.
+#include <stdio.h>
 
+int main() {
+    int n, first = 0, second = 1, next, i;
+
+    printf("Enter any number : ");
+    scanf("%d", &n);
+
+    printf("First %d Fibonacci series are: ", n);
+
+    for (i = 0; i < n; i++) {
+        if (i <= 1) {
+            next = i;
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+    return 0;
+}
